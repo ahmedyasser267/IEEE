23eeb462374b9f24a646156bc1b4fe0cf6b0f318
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2023-02-13 21:18:59 +0200

    Add files via upload

---
+++ Session 1/first.dart
@@ -0,0 +1,118 @@
+import 'dart:io';
+
+/*/
+varbiles --------
+out puts \inputs-------
+comments---------
+ope----
+fucation---
+
+ */
+int Sum(x, y){
+  var z =x+y;
+  return z;
+
+}
+int Sum1(x, y){
+  var z =x-y;
+  return z;
+
+}
+int Sum2(x, y){
+  var z =x*y;
+  return z;
+
+}
+int Sum4(x, y){
+  var z =x/y;
+  return z;
+
+}
+/*/
+
+ */
+void ope()
+{
+  print("Entre value");
+}
+
+
+
+
+
+void main(){
+
+ /*
+ var
+ string
+ dymanic
+int
+
+  */
+ /*
+var name=4;
+var nmae1 ="adh";
+var o =true;
+String name2= "ahmed";
+dynamic you =54;
+dynamic u =true;
+/*
+var vs dynamic
+ */
+var oo=4;
+//var oo=5;
+ dynamic pp=4;
+ pp=5;
+ int ppp= 44;
+ double p=4.556;
+ bool des= true;
+ */
+ /*
+ int x;
+ cin>>x;
+  */
+ /*
+
+  */
+ /*
+ out puts \inputs
+int ?x  =int.parse(stdin.readLineSync()!);
+print("the num is $x");
+String? y = stdin.readLineSync()!;
+print(y);
+
+  */
+/*
++ * / -
+ope
+ */
+/*
+int? x= int.parse(stdin.readLineSync()!);
+int? y= int.parse(stdin.readLineSync()!);
+var z= x+y;
+print(z);
+
+
+ */
+/*
+NUll saftey
+int? x =null;
+int? y ;
+print(y);
+
+ */
+
+  int? x= int.parse(stdin.readLineSync()!);
+  int? y= int.parse(stdin.readLineSync()!);
+  print(Sum(x, y));
+
+
+
+
+}
+
+
+
+
+
+
