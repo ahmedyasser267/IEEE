ad816c784d670306f5a9813b1c5148324e574ca4
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2023-02-17 21:55:20 +0200

    Create code.dart

---
+++ session 2/code.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+
+
+
+void main() {
+  /*
+  list
+   */
+  /*
+  var list1=[1,5,9,7,6,2,7];
+  var list2=["a",'d',"o","y"];
+  list1.add(10);
+  list1.insert(1, 26);
+  list1.remove(26);
+  list1.removeAt(5);
+  list1.removeRange(0, 5);
+  list1.removeLast();
+  print(list1.isEmpty);
+  print(list1.length);
+  print(list1.first);
+   */
+  //MAP
+  // KEY VALUE
+  /*
+  var map = {"ahmed": 19, "mohamed": 20, "assma": 15, "amr": 18};
+  //key names
+  print(map.keys);
+  print(map.values);
+  print(map.isEmpty);
+  print(map.isNotEmpty);
+  print(map.length);
+  print(map.runtimeType); // check type
+  var map1={"hassan":55,"heny":45};
+map.addAll(map1);
+map.clear();
+
+
+print(map);
+
+
+   */
+  //IF
+  /*
+  int? num=int.parse(stdin.readLineSync()!);
+  String? ope=stdin.readLineSync()!;
+  int? num2=int.parse(stdin.readLineSync()!);
+  if(ope=="+")
+    {
+      print(num+num2);
+    }
+  else if(ope=="-")
+    {
+      print(num-num2);
+    }
+
+   */
+  /*
+  var
+  string
+  int
+  double
+  dynamic
+   */
+
+
+
+
+
+
+
+}
