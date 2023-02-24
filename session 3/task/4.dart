889a993c49f260db0d1203eed61d95da7e760ac6
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2023-02-24 21:43:04 +0200

    Create 4.dart

---
+++ session 3/task/4.dart
@@ -0,0 +1,17 @@
+Given a number X. Determine if the number is prime or not
+
+Note:
+
+A prime number is a number that is greater than 1 and has only two factors which are 1 and itself.
+
+In other words : prime number divisible only by 1 and itself.
+
+Be careful that 1 is not prime .
+
+The first few prime numbers are
+  --------------
+  7
+  yes
+  -------------
+  6
+  no
