a35d0530f8042f63e2c74f1f9713fd80f066abd2
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2023-02-24 21:37:05 +0200

    Create code.dart

---
+++ session 3/code.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  /*
+  for(var i=1;i<=10;i++)
+    {
+      for(var j=1;j<=10;j++)
+        {
+          var z =i*j;
+          print("$i * $j =$z ");
+
+        }
+      /*
+      1*1= 1
+       */
+
+    }
+
+
+   */
+  /*
+  4! =4 *3 *2 *1
+
+   */
+  /*
+  int? num=int.parse(stdin.readLineSync()!);
+  var fact=1;
+  for(var i=1;i<=num;i++ )
+    {
+       fact =fact*i;
+    }
+  print(fact);
+
+
+   */
+  /*
+
+
+  int? row=int.parse(stdin.readLineSync()!);
+  for(var i =1;i<=row;i++)
+    {
+      for(var j =1;j<=i;j++)
+        {
+
+         stdout.write("*");
+        }
+      print("");
+    }
+
+
+   */
+  /*
+    stdout.write("*");
+    print();
+   */
+  /*
+ int? num =int.parse(stdin.readLineSync()!);
+ var  factorial =1;
+ while(num! >=1) {
+ factorial = factorial * num;
+   num--;
+ }
+ print("The factorial  is ${factorial}");
+
+}
+
+
+   */
+  /*
+  int num= int.parse(stdin.readLineSync()!);
+  var flag=0;
+  var m=num/2;
+  for(var i=2;i<m;i++)
+    {
+      if(num%i==0)
+        {
+          print("no prime");
+          flag =1;
+          break;
+        }
+      else
+        {
+          print("prime");
+        }
+
+    }
+
+
+   */
+
+}
+/*
+*
+**
+***
+****
+ */
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+
+ */
