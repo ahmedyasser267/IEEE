3cee7c6d33a4ffe8ed9928d582ebe5b6aa7eb438
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2023-02-24 21:40:10 +0200

    Create 1.dart

---
+++ session 3/task/1.dart
@@ -0,0 +1,16 @@
+first problem
+  
+  Given N numbers. Count how many of these values are even, odd, positive and negative.
+  First Line: "Even: X", where X is the number of even numbers in the given input
+ Second Line: "Odd: X", where X is the number of odd numbers in the given input.
+ Third Line: "Positive: X", where X is the number of positive numbers in the given input.
+ Fourth Line: "Negative: X", where X is the number of negative numbers in the given input.
+   
+  in put
+   5
+-5 0 -3 -4 12
+   out put
+ Even: 3
+Odd: 2
+Positive: 1
+Negative: 3
