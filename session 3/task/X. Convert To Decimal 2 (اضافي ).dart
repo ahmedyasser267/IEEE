9f11db332b336e769b0fd6defa43286835d2312e
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2023-02-24 21:47:42 +0200

    Create X. Convert To Decimal 2 (اضافي ).dart

---
+++ session 3/task/X. Convert To Decimal 2 (اضافي ).dart
@@ -0,0 +1,10 @@
+Given a number N. Print the result of doing the following operation on N:
+
+Convert N to its binary representation.
+Count number of ones in the above binary representation.
+Print the equivalent decimal number that its binary representation has only the number of ones that were counted above.
+For example: (10)decimal = (1010)binary has 2 ones "11", after converting "11" to decimal number it will become 3.
+  10
+  3
+  7
+  7
